1de8934f1fcc36858758b82631bd017e9b260424
Michelle <mlbagell@gmail.com>
2016-05-17 17:09:38 -0400

    added show erb and link pages Still have errors plus additional undefined method error in rspec tests

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,3 +1,11 @@
 get '/people' do
+  @people = Person.all
    erb :"/people/index"
+end
+
+get '/people/:id' do
+  @person = Person.find(params[:id])
+  @birth_date_number = Person.get_number(@person.birthdate.strftime("%m%d%Y"))
+  @message = Person.my_message(@birth_date_number)
+  erb :"/people/show"
 end
